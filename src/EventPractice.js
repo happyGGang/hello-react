7df6ac40a7af7b43a3186a19359c94081238c9a1
ykkang <ykkang@ocube.co.kr>
2022-12-19 20:33:51 +0900

    함수형 컴포넌트로 변경

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,44 +1,51 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 
-class EventPractice extends Component {
-  state = {
-    message: ''
-  }
-  handleClick = () => {
-    this.setState({
-      message: ''
-    });
+const EventPractice = () => {
+  const [message, setMessage] = useState('');
+  const [username, setUsername] = useState('');
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
   }
 
-  handleClick = () => {
-    alert(this.state.message);
-    this.setState({
-      message: ''
-    });
+  const handleMessage = (e) => {
+    setMessage(e.target.value);
+
   }
 
-  render() {
-    return (
-      <div>
-        <h1>Event Practice</h1>
-        <input
-          type="text"
-          name="message"
-          placeholder="아무거나 입력하세요"
-          value={this.state.message}
-          onChange={(e) => {
-              this.setState({
-                message: e.target.value
-              })
-            }
-          }
-        />
-        <button onClick={this.handleClick}>
-          확인
-        </button>
-      </div>
-    );
+  const handleClick = () => {
+    alert(username + ': ' + message);
+    setMessage('');
+    setUsername('');
   }
-}
+
+  const handleKeyPress = (e) => {
+    if(e.key === 'Enter') {
+      handleClick();
+    }
+  }
+
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="사용자명"
+        value={username}
+        onChange={handleUsername}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder="아무거나 입력해 보세요"
+        value={message}
+        onChange={handleMessage}
+        onKeyPress={handleKeyPress}
+      />
+      <button onClick={handleClick}>확인</button>
+    </div>
+  );
+};
 
 export default EventPractice;
