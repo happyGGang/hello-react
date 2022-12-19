1541d79421b8321839df39a2c4d7bc9f9951c21b
ykkang <ykkang@ocube.co.kr>
2022-12-19 19:38:00 +0900

    state에 input value 담기

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const EventPractice = () => {
-  return (
-    <div>
-      <h1>이벤트 연습</h1>
-    </div>
-  );
-};
+class EventPractice extends Component {
+  state = {
+    message: ''
+  }
+  handleClick = () => {
+    this.setState({
+      message: ''
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Event Practice</h1>
+        <input
+          type="text"
+          name="message"
+          placeholder="아무거나 입력하세요"
+          value={this.state.message}
+          onChange={(e) => {
+              this.setState({
+                message: e.target.value
+              })
+            }
+          }
+        />
+      </div>
+    );
+  }
+}
 
 export default EventPractice;
