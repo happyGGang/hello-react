dddda3936a7cfb1ebad0bbfabd4754102118d1bb
ykkang <ykkang@ocube.co.kr>
2022-12-22 13:09:45 +0900

    배열 추가 연습

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const EventPractice = () => {
-  const names =  ['a', 'b', 'c'];
-  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
+  const [names, setName] = useState([
+    {id: 1, name: 'name1'},
+    {id: 2, name: 'name2'},
+    {id: 3, name: 'name3'}
+  ]);
+  const [input, setInput] = useState('');
+  const [nextId, setNextId] = useState(4);
+
+  const nameList = names.map(name => <li key={name.id}>{name.name}</li>);
+
+  const onChange = (e) => {
+    setInput(e.target.value);
+  }
+
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      name: input
+    })
+    setNextId(nextId + 1);
+    setName(nextNames);
+    setInput('');
+  };
   return (
-    <ul>
-      {nameList}
-    </ul>
+    <>
+      <input
+        type="text"
+        value={input}
+        onChange={onChange}
+      />
+      <button onClick={onClick}>추가</button>
+      <ul>
+        {nameList}
+      </ul>
+    </>
   );
 };
 
